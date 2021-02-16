a16e6c6017262100d39ed3793eff199ab1e289bd
jiayao <jiayao3.14@outlook.com>
2021-02-16 14:32:58 +0800

    init

---
+++ src/main/java/vip/comic18/finder/FinderApplication.java
@@ -0,0 +1,13 @@
+package vip.comic18.finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinderApplication.class, args);
+	}
+
+}
