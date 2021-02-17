e4aa0dca989c21706a652702c439f31c7d682838
jiayao <jiayao3.14@outlook.com>
2021-02-17 20:06:04 +0800

    输出单独日志文件

--- src/main/java/vip/comic18/finder/controller/ComicController.java
+++ src/main/java/vip/comic18/finder/controller/ComicController.java
@@ -2,6 +2,7 @@ package vip.comic18.finder.controller;
 
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.HttpUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import java.util.concurrent.ExecutionException;
  * Created by jiayao on 2021/2/16.
  */
 @RestController
+@Slf4j
 public class ComicController {
     @Autowired
     private ComicService comicService;
@@ -22,13 +24,16 @@ public class ComicController {
     @GetMapping("/download")
     public String download(@RequestParam("homePage") String homePage) throws ExecutionException, InterruptedException {
         if(homePage == null) {
+            log.error("download->homePage为空");
             return "homePage为空";
         }
         if(!HttpUtil.isHttps(homePage) && !HttpUtil.isHttp(homePage)) {
+            log.error("download->homePage参数:[{}]并非http或https链接", homePage);
             return StrUtil.format("homePage参数:[{}]并非http或https链接", homePage);
         }
         ComicEntity comicInfo = comicService.getComicInfo(homePage);
         comicService.downloadComic(comicInfo);
-        return "下载完成";
+        log.info("下载[{}]完成", comicInfo.getTitle());
+        return StrUtil.format("下载[{}]完成", comicInfo.getTitle());
     }
 }
