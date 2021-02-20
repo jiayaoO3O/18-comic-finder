9b43a0563a244a5e8d8e18b70694e5380e7d0298
jiayao <jiayao3.14@outlook.com>
2021-02-20 17:28:36 +0800

    添加读取配置文件的url进行下载功能

--- src/main/java/vip/comic18/finder/runner/ComicRunner.java
+++ src/main/java/vip/comic18/finder/runner/ComicRunner.java
@@ -1,8 +1,14 @@
 package vip.comic18.finder.runner;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import vip.comic18.finder.entity.ComicEntity;
+import vip.comic18.finder.service.ComicService;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by jiayao on 2021/2/16.
@@ -10,8 +16,20 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class ComicRunner implements CommandLineRunner {
+    @Autowired
+    private ComicService comicService;
+    @Value("${comic.download.url}")
+    private String homePage;
+
     @Override
-    public void run(String... args) {
+    public void run(String... args) throws ExecutionException, InterruptedException {
         log.info("注意身体,适度看漫");
+        if(homePage == null) {
+            return;
+        }
+        ComicEntity comicInfo = comicService.getComicInfo(homePage);
+        log.info("开始下载[{}]:[{}]", comicInfo.getTitle(), homePage);
+        comicService.downloadComic(comicInfo);
+        log.info("下载[{}]完成", comicInfo.getTitle());
     }
 }
