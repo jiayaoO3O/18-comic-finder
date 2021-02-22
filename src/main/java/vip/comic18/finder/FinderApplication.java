313ce987599d034f322c5512feb702cd089eb7d8
jiayao <jiayao3.14@outlook.com>
2021-02-22 23:30:18 +0800

    测试github action下载功能,调整线程数

--- src/main/java/vip/comic18/finder/FinderApplication.java
+++ src/main/java/vip/comic18/finder/FinderApplication.java
@@ -2,7 +2,12 @@ package vip.comic18.finder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 @EnableAsync
@@ -12,4 +17,19 @@ public class FinderApplication {
 		SpringApplication.run(FinderApplication.class, args);
 	}
 
+	@Bean()
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+		taskExecutor.setCorePoolSize(4);
+		taskExecutor.setMaxPoolSize(8);
+		taskExecutor.setQueueCapacity(256);
+		taskExecutor.setKeepAliveSeconds(60);
+		taskExecutor.setThreadNamePrefix("async-task-");
+		taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+		taskExecutor.setAllowCoreThreadTimeOut(true);
+		taskExecutor.setAwaitTerminationSeconds(60);
+		taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+		taskExecutor.initialize();
+		return taskExecutor;
+	}
 }
