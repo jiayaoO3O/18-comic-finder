4894bed61f015182cff8ea0cb343f9ebf98ab509
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2023-03-31 08:55:16 +0800

    cs

--- py/jm.py
+++ py/jm.py
@@ -3,4 +3,6 @@ import sys
 
 sys.path.append("18-comic-finder/py/")
 
+jmcomic.option(f'./py/jmcomic_config_no_proxy.yml')
 jmcomic.download_album('430740')
+# jmcomic.option(f'./py/jmcomic_config_no_proxy.yml')
